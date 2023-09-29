76d924cf29f5e307e26bc1b8cb0f057df420b9dd
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-09-29 13:07:07 +0200

    feat: added output of character details

--- src/components/CharacterDetailCard.tsx
+++ src/components/CharacterDetailCard.tsx
@@ -1,15 +1,39 @@
 import {useParams} from "react-router-dom";
-import {Character} from "./Types.tsx";
+import {CharacterDetails} from "./Types.tsx";
 
 type CharacterDetailCardProps = {
-    characters: Character[]
+    characters: CharacterDetails[]
 }
 
 export default function CharacterDetailCard( props: CharacterDetailCardProps ) {
     const urlParams = useParams();
+    const idStr = urlParams.id;
+    if (!idStr)
+        return <>Can't show chartacter details.<br/>No ID specified.</>
+
+    const id:number = parseInt(idStr);
+    const foundCharacters = props.characters.filter(ch => ch.id === id);
+    if (foundCharacters.length<1)
+        return <>Can't show chartacter details.<br/>Can't find character with this ID.</>
+
+    const foundCharacter = foundCharacters[0];
+
     return (
-        <>
-            Details for charcter with ID {urlParams.id}
-        </>
+        <div>
+            <div>id      : {foundCharacter.id      }</div>
+            <div>name    : {foundCharacter.name    }</div>
+            <div>gender  : {foundCharacter.gender  }</div>
+            <div>status  : {foundCharacter.status  }</div>
+            <div>type    : {foundCharacter.type    }</div>
+            <div>species : {foundCharacter.species }</div>
+            <div>origin  : {foundCharacter.origin.name  }</div>
+            <div>location: {foundCharacter.location.name }</div>
+            <div>created : {foundCharacter.created }</div>
+            {
+                foundCharacter.image
+                    ? <div><img alt={"Image of "+foundCharacter.name} src={foundCharacter.image}/></div>
+                    : ""
+            }
+        </div>
     )
 }
--- src/components/Types.tsx
+++ src/components/Types.tsx
@@ -19,3 +19,23 @@ export type CharacterResponse = {
     results: Character[]
 }
 
+export type Location = {
+    name: string
+    url: string
+}
+
+export type CharacterDetails = {
+    id: number
+    name: string
+    status: string
+    species: string
+    type: string
+    gender: string
+    origin: Location
+    location: Location
+    image: string,
+    episode: string[]
+    url: string,
+    created: string
+}
+
